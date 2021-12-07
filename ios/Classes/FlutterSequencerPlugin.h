53d1f7d6c6eb2b6c4cee52dc1029458373dc2c17
Mike Elson <mike@voicelessons.com>
2021-12-06 20:01:30 -0800

    add void param

--- ios/Classes/FlutterSequencerPlugin.h
+++ ios/Classes/FlutterSequencerPlugin.h
@@ -5,18 +5,18 @@
 
 
 void setup_engine(int64_t sampleRateCallbackPort);
-void destroy_engine();
+void destroy_engine(void);
 void add_track_sfz(char *sfzPath, char *tuningPath, int64_t callbackPort);
 void add_track_sfz_string(char *sampleRoot, char *sfzString, int64_t callbackPort);
 void add_track_sf2(char *path, bool isAsset, int presetIndex, int64_t callbackPort);
 void remove_track(int trackIndex);
 void reset_track(int trackIndex);
-UInt32 get_position();
+UInt32 get_position(void);
 Float32 get_track_volume(int trackIndex);
-UInt64 get_last_render_time_us();
+UInt64 get_last_render_time_us(void);
 UInt32 get_buffer_available_count(int trackIndex);
 void handle_events_now(int trackIndex, UInt8 *eventData, UInt32 eventsCount);
 UInt32 schedule_events(int trackIndex, UInt8 *evemtData, UInt32 eventsCount);
 void clear_events(int trackIndex, int fromFrame);
-void engine_play();
-void engine_pause();
+void engine_play(void);
+void engine_pause(void);
